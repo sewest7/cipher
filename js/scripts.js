42c9a149e3d6f99ff3f2f45389ef7410298e1bd7
sewest7 <seth.west7@gmail.com>
2022-01-10 14:41:40 -0800

    Add sentence prompt and firstLast function in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,17 @@
 //(not a function)prompt user to enter a sentence and print sentence to console
 
-
+let sentencePrompt = prompt("Enter a sentence. Make it really cool!");
 
 //return first and last letter, capitalizes and return sentence as new string
 
-
+function firstLast(sentence){
+    let firstChar = sentence.charAt(0);
+    let lastChar = sentence.charAt(sentence.length - 1);
+    let nextSentence = (firstChar + lastChar).toUpperCase();
+    return nextSentence;
+  }
+  
+  alert(firstLast(sentencePrompt));
 //take return value of first function, and reverse.
 
 
