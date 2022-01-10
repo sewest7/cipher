1784bf77d14504ed93fc3e89f121c0f6c57072f4
sewest7 <seth.west7@gmail.com>
2022-01-10 14:22:26 -0800

    Start project. Add bootstrap, jsquery.

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+//(not a function)prompt user to enter a sentence and print sentence to console
+
+
+
+//return first and last letter, capitalizes and return sentence as new string
+
+
+//take return value of first function, and reverse.
+
+
+//call first two functions and concatenate first sentence and second sentence 
+
+
+
+//count number of letters in original sentence, divide by two, and output letter at result's index then concat at beginning
+
+
+//Bonus - reverse return from fourth function
